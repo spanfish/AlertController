9efcfc0d5291ab25a7f8aa1b06349f870aeefdfb
XIANGWEI WANG <xiangwei.wang@dji.com>
2017-01-13 10:21:53 +0900

    init
    init

---
+++ UIAlertController+Window.h
@@ -0,0 +1,15 @@
+//
+//  UIAlertController+Window.h
+//  GSPro
+//
+//  Created by xiangwei wang on 2016/11/01.
+//
+
+#import <UIKit/UIKit.h>
+#import <objc/runtime.h>
+
+@interface UIAlertController (Window)
+
+- (void)show;
+- (void)show:(BOOL)animated;
+@end
